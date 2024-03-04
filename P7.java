4726121ccc2fd175266a07c3fa6a4ceee1deb28e
Vatsal Shah <161283745+csevatsalshah@users.noreply.github.com>
2024-03-04 15:18:18 +0530

    Practical - 7
    Practical 7 - GTU - OOP - JAVA

--- P7.java
+++ P7.java
@@ -1,17 +1,15 @@
-public class Pract7 {
+public class P7 {
+    public static void main(String[] args) {
+    
+        int letter1 = 'A' + (int)(Math.random() * ('Z' - 'A'));
+        int letter2 = 'A' + (int)(Math.random() * ('Z' - 'A'));
+        int letter3 = 'A' + (int)(Math.random() * ('Z' - 'A'));
 
-	public static void main(String[] args) {
-		
-		
-		String plateNumber = ""+(char)(Math.random()*26+65);
-		plateNumber += (char)(Math.random()*26+65);
-		plateNumber += (char)(Math.random()*26+65);
-		plateNumber += (int)(Math.random()*10);
-		plateNumber += (int)(Math.random()*10);
-		plateNumber += (int)(Math.random()*10);
-		plateNumber += (int)(Math.random()*10);
-		System.out.println(plateNumber);
-		
-	}
+        int num1 = (int)(Math.random() * 10);
+        int num2 = (int)(Math.random()* 10);
+        int num3 = (int)(Math.random()* 10);
+        int num4 = (int)(Math.random()* 10);
 
+        System.out.println("" + (char)(letter1) + (char)(letter2) + (char)(letter3) + (int)(num1)+ (int)(num2)+ (int)(num3)+ (int)(num4));
+    }
 }
